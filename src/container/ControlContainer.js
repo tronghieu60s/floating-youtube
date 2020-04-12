3fe0bcf2636c01f033b7d57239949fb618b3be41
Trọng Hiếu <tronghieu60s@gmail.com>
2020-04-12 11:10:16 +0700

    confirm quit app

--- src/container/ControlContainer.js
+++ src/container/ControlContainer.js
@@ -8,7 +8,9 @@ import tabsIcon from '../icons/tabs.svg';
 const electron = window.require('electron');
 
 function ControlContainer(props) {
-    var window = electron.remote.getCurrentWindow();
+    let window = electron.remote.getCurrentWindow();
+    const { dialog } = electron.remote;
+
     let { darkMode } = props;
     const [floating, setFloating] = useState(false);
     window.setAlwaysOnTop(floating, "floating");
@@ -26,7 +28,18 @@ function ControlContainer(props) {
                 <button onClick={() => window.minimize()} type="button" className="btn btn-primary btn-sm">
                     <img className="controls-window-icon" src={minimizeIcon} alt={minimizeIcon} />
                 </button>
-                <button onClick={() => window.close()} type="button" className="btn btn-danger btn-sm">
+                <button onClick={() => {
+                    let options = {
+                        type: "warning",
+                        title: "Floating Youtube",
+                        buttons: ["Yes", "No"],
+                        message: "Do you really want to quit?"
+                    }
+                    let response = dialog.showMessageBox(options)
+                    response.then(res => {
+                        if(res.response === 0) window.close();
+                    })
+                }} type="button" className="btn btn-danger btn-sm">
                     <img className="controls-window-icon" src={closeIcon} alt={closeIcon} />
                 </button>
             </div>
--- src/container/WelcomeContainer.js
+++ src/container/WelcomeContainer.js
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 import Welcome from '../components/Welcome';
 let { version } = require('../../package.json');
 
+const electron = window.require('electron');
+
 function WelcomeContainer() {
+    const { dialog } = electron.remote;
     const [welcome, setWelcome] = useState(() => {
         let welcomeSession = JSON.parse(sessionStorage.getItem("welcome")) || { status: true };
         return welcomeSession.status;
     });
 
     useEffect(() => {
+        // let options  = {
+        //     buttons: ["Yes","No","Cancel"],
+        //     message: "Do you really want to quit?"
+        //    }
+        // dialog.showMessageBox(options)
+
         let welcomeTime = setTimeout(() => {
             sessionStorage.setItem("welcome", JSON.stringify({ status: false }));
             setWelcome(false);
